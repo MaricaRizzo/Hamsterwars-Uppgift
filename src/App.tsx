5cc8c7c67d8efd9984071fde2b1cf5399422704d
MaricaRizzo <maricarizzoo@gmail.com>
2021-10-29 13:07:35 +0200

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Hamsterwars</h1>¨
+        <nav>
+          Router links
+        </nav>
+      </header>
+      <main>
+        Switch components
+      </main>
+    </div>
+  );
+}
+
+export default App;
