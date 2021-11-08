5b46b65fc6e9071709def19ade7dec4c0330013e
MaricaRizzo <maricarizzoo@gmail.com>
2021-11-08 16:40:31 +0100

    heroku fix 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use( '/', express.static(__dirname + '../../src'))
+app.use( '/', express.static(__dirname + '../../public'))
 app.use('/img', express.static(__dirname + '/../hamsters'))
 
 
